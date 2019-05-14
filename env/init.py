cc8170495f1df0b9ab51d11baec6bad3826b0bc0
Kidon Seo <kidonseo@gmail.com>
2019-05-14 13:14:03 +0900

    initialize the server

--- env/init.py
+++ env/init.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return "hello Flask"
+@app.route('/', methods = ['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Try again'
+        else:
+            return redirect(url_for('secret'))
+    return render_template('index.html',error=error)
+
+@app.route('/secret')
+def secret():
+    return "Secret"
 
 if __name__ == "__main__":
   app. run()
